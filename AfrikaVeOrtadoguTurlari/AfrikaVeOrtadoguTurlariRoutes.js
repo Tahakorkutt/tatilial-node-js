0cecce1f4e2d9575c80ca5acb0b8c9a4b238e17f
İSMAİL TAHA KORKUT <111304410+Tahakorkutt@users.noreply.github.com>
2025-09-01 11:01:13 +0300

    asda

---
+++ AfrikaVeOrtadoguTurlari/AfrikaVeOrtadoguTurlariRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllTurlar,
+  getTuruById,
+  createTuru,
+  updateTuru,
+  deleteTuru,
+} = require("../controllers/AfrikaVeOrtadoguTurlari/AfrikaVeOrtadoguTurlariController.js");
+
+router.get("/", getAllTurlar);
+router.get("/:id", getTuruById);
+router.post("/", createTuru);
+router.put("/:id", updateTuru);
+router.delete("/:id", deleteTuru);
+
+module.exports = router;
